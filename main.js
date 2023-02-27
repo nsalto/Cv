29933aa8e03b30c84201ff1e21d951d6c738c3d9
nsalto <nahuelsalto.71@gmail.com>
2023-02-27 00:06:07 -0300

    update

--- main.js
+++ main.js
@@ -2,34 +2,34 @@ const nombre = "Maximiliano Nahuel Salto"
 
 const experiencia = [
     {
+        lugar: "Cocos Capital",
+        puesto: "Backend Developer",
+        periodo: "Mar 2022 - present",
+        descripción: "As a Backend Developer, I am responsible for maintaining and developing new features for a financial application. I work with the team to understand user requirements, and I implement new functionality while maintaining code quality and best practices. Additionally, I have experience in automating processes in different areas of the company, streamlining operations and increasing efficiency. Overall, my goal is to deliver high-quality solutions that meet business requirements and provide value to our users."
+    },
+    {
         lugar: "Coca-Cola Andina",
         puesto: "Solution Developer",
-        periodo: "Jun 2021 - Actualidad",
-        descripción: "Desarrollo y mantenimiento de Web apps y Mobile Apps, planificacion y diseño de nuevas funcionalidades y mejoras."
+        periodo: "Jun 2021 - Mar 2022",
+        descripción: "Planning, designing and developing and maintaining web and mobile applications"
     },
     {
         lugar: "Ross Outside the Box",
         puesto: "Fullstack develop Jr",
-        periodo: "Dic 2020 - Jun 2021",
-        descripción: "Fullstack en Ross outside the box, creación e implementación de software tanto en backend como en frontend"
+        periodo: "Nov 2020 - Jun 2021",
+        descripción: "Fullstack Nodejs, Angular 7+, Vuejs, developing and maintaining web and mobile applications"
     },
     {
         lugar: "Ross Outside the Box",
         puesto: "QA Automation",
-        periodo: "Ago 2020 - Dic 2020",
-        descripción: "Quality Assurance en Ross outside the box, Crear planes de prueba y testing, automatizaciones, mejorar estrategias de prueba y asegurar la calidad del software"
-    },
-    {
-        lugar: "Konecta Argentina",
-        puesto: "Analista de Datos",
-        periodo: "Feb 2018 -  Oct 2020",
-        descripción: "Automatizaciones JS, informes, mediciones, gráficos para el seguimiento de las actividades, y detectar falencias y/o posibilidad de optimizaciones de tareas en diversas áreas."
+        periodo: "Jul 2020 - Nov 2020",
+        descripción: "Creating test plans, performing testing and automation using Pyhton, improving testing strategies to ensure software quality"
     }
 ];
 
 const estudios = [{
     lugar: "Facultad de Matemática, Astronomía, Física y Computación (UNC)",
-    periodo: "2016-Actualidad",
+    periodo: "2016-present",
     descripción: "Licenciatura en ciencias de la Computación"
     }
 ];
@@ -40,24 +40,8 @@ const lenguajesActuales = [
         img:"https://raw.githubusercontent.com/devicons/devicon/2809b567852a4648062a2d3e7c1c531367458c0b/icons/nodejs/nodejs-original.svg"
     },
     {
-        name: "Vuejs",
-        img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original.svg"
-    },
-    {
-        name: "Angular 7+",
-        img:"https://raw.githubusercontent.com/devicons/devicon/2809b567852a4648062a2d3e7c1c531367458c0b/icons/angularjs/angularjs-plain.svg"
-    },
-    {
-        name: "JavaScript",
-        img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg"
-    },
-    {
-        name: "HTML",
-        img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg"
-    },
-    {
-        name: "CSS",
-        img:"https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg"
+        name: "Typescript",
+        img: "https://raw.githubusercontent.com/devicons/devicon/7a4ca8aa871d6dca81691e018d31eed89cb70a76/icons/typescript/typescript-original.svg",
     },
     {
         name: "GIT",
@@ -68,40 +52,15 @@ const lenguajesActuales = [
         img:"https://raw.githubusercontent.com/jmnote/z-icons/master/svg/python.svg"
     },
     {
-        name: "MySQL",
-        img:"https://raw.githubusercontent.com/devicons/devicon/2809b567852a4648062a2d3e7c1c531367458c0b/icons/mysql/mysql-plain.svg"
-    } 
-    
-]
-
-const lenguajesAprendiendo = [
-    {
-        name: "Angular 7+",
-        img: "https://raw.githubusercontent.com/devicons/devicon/2809b567852a4648062a2d3e7c1c531367458c0b/icons/angularjs/angularjs-plain.svg",
-    },
-    {
-        name: "MongoDB",
-        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg"
-    },
-    {
-        name: "Typescript",
-        img: "https://raw.githubusercontent.com/devicons/devicon/7a4ca8aa871d6dca81691e018d31eed89cb70a76/icons/typescript/typescript-original.svg",
-    },
-    {
-        name: "Web 3.0",
-        img: "https://raw.githubusercontent.com/vorillaz/devicons/ba75593fdf8d66496676a90cbf127d721f73e961/!SVG/w3c.svg",
-    },
-    {
-        name: "Docker",
-        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original.svg",
+        name: "Postgres",
+        img: "https://www.vectorlogo.zone/logos/postgresql/postgresql-icon.svg"
     },
     {
         name: "Amazon Web Service",
         img: "https://raw.githubusercontent.com/devicons/devicon/2809b567852a4648062a2d3e7c1c531367458c0b/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg"
     }
-        
+    
 ]
-
 // Cambia el nombre del H1 que con tiene el nombre del usuario
 var nombreDOM = document.getElementById("name")
 nombreDOM.innerText = nombre
